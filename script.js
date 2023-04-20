572741938e6353501d95cbf72dddf2e4babb316e
Beatriz Rivard <beatrizfreire182@gmail.com>
2023-04-20 11:22:41 -0400

    new changes

--- script.js
+++ script.js
@@ -4,19 +4,26 @@ const apiKey = "3da186cea02cf61bf87b0a2d58d7736d";
 const cityInput = document.getElementById("city-input");
 const searchBtn = document.getElementById("click-btn");
 const weatherList = document.querySelectorAll(".list-group");//lista da temp semana
-
+const weatherIconElement = document.querySelector("#weather-icon");//new
+const umidityElement = document.querySelector("#umidity span");//new
+const windElement = document.querySelector("#wind span");//new
 
 //function
-const getWeatherData =  async(city) => {
+const getWeatherData =  async (city) => {
 
-  const apiKey = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
-  const res = await fetch(apiKey) 
+  const apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`
+  const res = await fetch(apiURL) 
   const data = await res.json()
-  console.log(data)
+  return data
 };
 
-const showWeatherData = (city) => {
-  console.log(city)
+const showWeatherData = async (city) => {
+  const data =  await getWeatherData(city);
+
+  cityElement.innerText = data.name
+  tempElement.innerText = parseInt(data.main.temp)
+  descElement.innerText = data.weather[0].description
+  weatherIconElement.setAttribute("src", `http://api.openweathermap.org/img/wn/${data.weather[0].icon}.png`);
 };
 
 //event
@@ -25,7 +32,6 @@ searchBtn.addEventListener("click", async (e) => {
 
   const city = cityInput.value;
 
-  showWeatherData(city)
+  getWeatherData(city)
 });
-
 
